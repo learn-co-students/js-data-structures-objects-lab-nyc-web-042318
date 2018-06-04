fa1486a72c7aeaa022fc9e0f7b849c88f7b14ea5
sgg2123 <sgg2123@columbia.edu>
2018-06-04 16:41:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+const driver = { name: 'Mac' };
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({}, driver, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(driver, { [key]: value });
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const newObject = {...driver};
+  delete newObject[key];
+  return newObject;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
